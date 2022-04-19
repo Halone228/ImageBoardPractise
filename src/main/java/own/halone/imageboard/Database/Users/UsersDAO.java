334148a76a314a7743d1e1e7813fa2ca31a7576c
Halone <randomnik192@gmail.com>
2022-04-20 00:45:24 +0300

    Added Security:  Authorization  Registration  Some changes in front

--- src/main/java/own/halone/imageboard/Database/Users/UsersDAO.java
+++ src/main/java/own/halone/imageboard/Database/Users/UsersDAO.java
@@ -1,8 +1,14 @@
 package own.halone.imageboard.Database.Users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsersDAO extends JpaRepository<Users,Long> {
+    @Query("select u from Users u where u.username = :name")
+    Users findByUsername(@Param("name") String name);
+    @Query("select u from Users u where u.email = :email")
+    Users findByEmail(@Param("email") String email);
 }
